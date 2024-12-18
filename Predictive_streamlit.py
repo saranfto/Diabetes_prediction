198fb36195b6200555073657d2f128253b8b5e4c
saranfto <122554073+saranfto@users.noreply.github.com>
2024-12-18 08:04:44 +0530

    Update Predictive_streamlit.py

--- Predictive_streamlit.py
+++ Predictive_streamlit.py
@@ -5,6 +5,7 @@ import streamlit as st
 
 # loading the saved model
 loaded_model = pickle.load(open("trained_model.sav", 'rb'))
+Stool = StandardScaler()
 
 #tuning
 def t1():
@@ -35,8 +36,9 @@ def diabetes_prediction(input_data):
 
     # reshape the array as we are predicting for one instance
     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
-    prediction = loaded_model.predict(input_data_reshaped)
+    Stooled_data = Stool.transform(input_data_reshaped)
+    # print(Stooled_data)
+    prediction = loaded_model.predict(Stooled_data)
     print(prediction)
 
     if (prediction[0] == 0):
